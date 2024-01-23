f29cc53d8ae79b32dea673887bb282cddf78ac2f
ThanhThy <thy.nnt.63cntt@ntu.edu.vn>
2024-01-23 23:14:20 +0700

    Ví dụ đầu
    Chỉ để

---
+++ HelloWorld/src/tiilthy/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiilthy.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý nguồn với GIT");
+	}
+
+}
